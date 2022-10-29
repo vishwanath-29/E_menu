cdb1992e836374376eda1c10bf9637b2dcadb576
Vishwa <vishwanathnarayanan29@gmail.com>
2022-10-29 23:32:46 +0530

    Added Edit and Delete Option, Changes reflect to DB

--- Add_Edit_Items/views.py
+++ Add_Edit_Items/views.py
@@ -37,11 +37,41 @@ def edit(request):
     all_data = list(Food.objects.all().values())
     print(all_data)
     if request.method=="POST":
-        print(request)
-        get_food_id=request.POST['FoodId']
-        print(get_food_id)
-        food_data = Food.objects.filter(id=get_food_id).values()
-        context=food_data.values().first()
-        return render(request,"add_edit/edit.html",context)
-    
+        if "goto_edit" in request.POST:
+            print(request)
+            get_food_id=request.POST['FoodId']
+            print(get_food_id)
+            food_data = Food.objects.filter(id=get_food_id).values()
+            context=food_data.values().first()
+            return render(request,"add_edit/edit.html",context)
+        elif "Save" in request.POST:
+            get_food_id=request.POST['FoodId']
+            food_name=request.POST['FoodName']
+            food_description=request.POST['ShortDescription']
+            food_isavailable=request.POST['isvegetarian']
+            food_isbestseller=request.POST['isbestseller']
+            food_isvegetarian=request.POST['isavailable']
+            food_price=request.POST['price']
+            food_category=request.POST['category']
+            food_image_link=request.POST['food_link']
+
+            food=Food.objects.get(id=get_food_id)
+            food.Food_Name=food_name
+            food.Short_Description=food_description
+            food.is_veg=food_isvegetarian
+            food.is_bestseller=food_isbestseller
+            food.is_available=food_isavailable
+            food.Price=food_price
+            food.Category=food_category
+            food.image_link=food_image_link
+            
+            food.save()
+            return render(request,"add_edit/edit.html")   
+        elif "Delete" in request.POST:
+            get_food_id=request.POST['FoodId']
+            food=Food.objects.get(id=get_food_id)
+            food.delete()
+            return render(request,"add_edit/edit.html")  
+
+        
     return render(request,"add_edit/card_page.html",{"food_details":all_data})   
